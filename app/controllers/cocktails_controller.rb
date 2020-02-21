6646c33f2469907c6ea21a45fc740e9a05a5fd79
Mohammad Mirza <Hammad_Senior@hotmail.com>
2020-02-21 15:12:02 -0500

    made numerous updates

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,6 +24,6 @@ class CocktailsController < ApplicationController
   private
 
 def safe_params
-  params.require(:cocktail).permit(:name)
+  params.require(:cocktail).permit(:name, :photo)
 end
 end
